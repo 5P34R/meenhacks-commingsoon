9320a525a94fd1462f94630a90a359500665fea0
College of Engineering Kidangoor CAPE <vk2345732@gmail.com>
2023-03-14 19:25:49 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <div className="App bg-[#1E1E1E] h-screen w-full">
+      <div className="flex justify-center items-start">
+        
+      </div>
+    </div>
+  )
+}
+
+export default App
