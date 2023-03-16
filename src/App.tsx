ab8c004200ee095ece396eb0148e3422ebfe9d20
VishDroid-dev <vk2345732@gmail.com>
2023-03-16 10:34:23 +0530

    changed the moon reduced the size

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import moon from './assets/moon.gif'
+import moon1 from './assets/moon1.gif'
 
 function App() {
 
   return (
     <div className="App bg-black h-screen w-full">
       <div className="flex justify-center items-center h-screen">
-        <img src={moon} alt="GIF" className="max-w-full max-h-full" />
+        <img src={moon1} alt="GIF" className="max-w-full max-h-full" />
       </div>
     </div>
   )
